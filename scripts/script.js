dc4f22f36b1497cc20053c373f4d4a461855629d
Redd <piihlsonyt@gmail.com>
2024-02-26 09:37:18 +0100

    Update image size and position in slideshow

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,13 @@
 let dotContainer = document.querySelector('#dot-container');
 let slideShow = document.querySelector('.slideshow-container');
 
-// for each image in the carousel, create a dot and append it to the dot container div
-for (let i = 0; i < slideShow.children.length; i++) {
-  let dot = document.createElement('span');
-  dot.classList.add('dot');
-  dotContainer.appendChild(dot);
+createDots();
+
+function createDots() {
+  // for each image in the carousel, create a dot and append it to the dot container div
+  for (let i = 0; i < slideShow.children.length; i++) {
+    let dot = document.createElement('span');
+    dot.classList.add('dot');
+    dotContainer.appendChild(dot);
+  }
 }
